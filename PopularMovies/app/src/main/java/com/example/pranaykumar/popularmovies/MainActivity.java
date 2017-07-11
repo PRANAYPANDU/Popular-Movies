00702b6892dace594892ae3fbd39764b7cb13db0
Anugurti Pranaykumar <pranaypandu.225@gmail.com>
2017-07-11 18:41:59 +0530

    Added Search Movie option

--- PopularMovies/app/src/main/java/com/example/pranaykumar/popularmovies/MainActivity.java
+++ PopularMovies/app/src/main/java/com/example/pranaykumar/popularmovies/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pranaykumar.popularmovies;
 import android.app.LoaderManager;
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
@@ -7,26 +8,30 @@ import android.databinding.DataBindingUtil;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import com.example.pranaykumar.popularmovies.databinding.ActivityMainBinding;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Movie>>{
 
   private static final int MOVIES_LOADER_ID=1;
+  int isPopularMovies=1;
   ActivityMainBinding mainBinding;
-
   //URL for popular movies,insert your API key in place of YourAPIKey
-  private static final String PopularmoviesDB_REQUEST_URL="http://api.themoviedb.org/3/movie/popular?api_key=857710a9c17b11d80aa32f98d00aa936";
+  private static final String POPULARMOVIES_DB_REQUEST_URL ="http://api.themoviedb.org/3/movie/popular?api_key=857710a9c17b11d80aa32f98d00aa936";
   //URL for top rated movies,insert your API key in place of YourAPIKey
-  private static final String top_ratedMoviesDB_REQUEST_URL="http://api.themoviedb.org/3/movie/top_rated?api_key=857710a9c17b11d80aa32f98d00aa936";
+  private static final String TOP_RATED_MOVIES_DB_REQUEST_URL ="http://api.themoviedb.org/3/movie/top_rated?api_key=857710a9c17b11d80aa32f98d00aa936";
 
-  private String final_url=PopularmoviesDB_REQUEST_URL;
+  private String final_url= POPULARMOVIES_DB_REQUEST_URL;
 
   ArrayList<Movie> movies;
 
@@ -39,13 +44,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
   protected void onCreate(Bundle savedInstanceState) {
 
     if(getIntent()!=null){
-      Intent intent=getIntent();
+     Intent intent=getIntent();
       int i=intent.getFlags();
       if(i==2){
-        final_url=top_ratedMoviesDB_REQUEST_URL;
+        final_url= TOP_RATED_MOVIES_DB_REQUEST_URL;
         setTitle(R.string.sortBy_top_rated_Movies);
       }
+
     }
+
+
+
     movies=new ArrayList<Movie>();
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
@@ -87,6 +96,25 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
   }
 
+  private void doMySearch(String query) {
+    Log.d("O_MY","Searched mivie name is:"+query);
+    ConnectivityManager connMgr = (ConnectivityManager) getSystemService(
+        Context.CONNECTIVITY_SERVICE);
+    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+    if (networkInfo != null && networkInfo.isConnected()) {
+      //Get a reference to the LoaderManager,in order to interact with loaders
+
+      android.app.LoaderManager loaderManager=getLoaderManager();
+      // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+      // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
+      // because this activity implements the LoaderCallbacks interface).
+      final_url="https://api.themoviedb.org/3/search/movie?api_key=857710a9c17b11d80aa32f98d00aa936&query="+query;
+      //setTitle("Results for "+query);
+      loaderManager.destroyLoader(MOVIES_LOADER_ID);
+      loaderManager.initLoader(MOVIES_LOADER_ID,null,this);
+    }
+  }
+
   @Override public Loader<ArrayList<Movie>> onCreateLoader(int id, Bundle args) {
 
     return new MoviesLoader(this,final_url);
@@ -97,7 +125,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     mainBinding.loadingIndicator.setVisibility(View.GONE);
     // Set empty state text to display "No Movies found."
     mainBinding.emptyView.setText(R.string.no_movies);
-
     if(data!=null&&!data.isEmpty()){
       Log.d("O_MY", String.valueOf(data.size()));
       mMoviesAdapter.setmMoviesData(data);
@@ -113,8 +140,53 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     //inflate the menu options from the res/menu/sort.xml
     //This adds menu items to the app bar.
     getMenuInflater().inflate(R.menu.sort,menu);
+
+    //Adding search bar
+    getMenuInflater().inflate(R.menu.search,menu);
+    SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+    SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+    EditText searchEditText = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+    searchEditText.setHint("Search for a Movie");
+    searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+    searchView.setOnQueryTextListener(new OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String query) {
+        doMySearch(query);
+        return true;
+      }
+
+      @Override
+      public boolean onQueryTextChange(String newText) {
+        return true;
+      }
+    });
+    MenuItemCompat.setOnActionExpandListener(menu.findItem(R.id.action_search), new MenuItemCompat.OnActionExpandListener() {
+      @Override
+      public boolean onMenuItemActionExpand(MenuItem item) {
+        return true;
+      }
+
+      @Override
+      public boolean onMenuItemActionCollapse(MenuItem item) {
+        // Write your code here
+        backToMain(isPopularMovies);
+        return true;
+      }
+
+
+    });
     return true;
   }
+
+  private void backToMain(int isPopularMovies) {
+    if(isPopularMovies==1)
+      final_url= POPULARMOVIES_DB_REQUEST_URL;
+    else
+      final_url= TOP_RATED_MOVIES_DB_REQUEST_URL;
+
+    getLoaderManager().restartLoader(MOVIES_LOADER_ID, null,this);
+  }
+
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -122,18 +194,21 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     switch (item.getItemId()){
       //Respond to a click on the "Popular Movies" menu option
       case R.id.sortBy_popularMovies:
-        final_url=PopularmoviesDB_REQUEST_URL;
+        final_url= POPULARMOVIES_DB_REQUEST_URL;
+        isPopularMovies=1;
         getLoaderManager().restartLoader(MOVIES_LOADER_ID, null,this);
         setTitle(getString(R.string.app_name));
         return true;
       case R.id.sortBy_top_rated_Movies:
-        final_url=top_ratedMoviesDB_REQUEST_URL;
+        final_url= TOP_RATED_MOVIES_DB_REQUEST_URL;
+        isPopularMovies=0;
         getLoaderManager().restartLoader(MOVIES_LOADER_ID, null,this);
         setTitle(getString(R.string.sortBy_top_rated_Movies));
         return true;
       case R.id.sortBy_favourite_Movies:
         Intent favIntent=new Intent(MainActivity.this,FavouriteMoviesActivity.class);
         startActivity(favIntent);
+        break;
     }
     return super.onOptionsItemSelected(item);
   }
--- PopularMovies/app/src/main/java/com/example/pranaykumar/popularmovies/MovieDetailsActivity.java
+++ PopularMovies/app/src/main/java/com/example/pranaykumar/popularmovies/MovieDetailsActivity.java
@@ -1,8 +1,8 @@
 package com.example.pranaykumar.popularmovies;
 
+
 import static com.example.pranaykumar.popularmovies.R.drawable.red_circle;
 import static com.example.pranaykumar.popularmovies.R.drawable.white_circle;
-
 import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.Context;
@@ -36,12 +36,10 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ShareActionProvider;
 import android.widget.Toast;
-
 import com.example.pranaykumar.popularmovies.data.PopularMoviesContract.FavouriteMoviesEntry;
 import com.example.pranaykumar.popularmovies.data.PopularMoviesDbHelper;
 import com.example.pranaykumar.popularmovies.databinding.ActivityMovieDetailsBinding;
 import com.squareup.picasso.Picasso;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -52,12 +50,9 @@ import java.util.logging.LogRecord;
 public class MovieDetailsActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<ArrayList<String >>>{
 
   ActivityMovieDetailsBinding movieDetailsBinding;
-
-
   private String moviesDbURL="http://api.themoviedb.org/3/";
   private String apiKey="?api_key=857710a9c17b11d80aa32f98d00aa936";
   private static final int LOADER_ID = 1;
-
   ArrayList<String> sTrailers;
   ArrayList<String> sReviews;
   String videosURL;
@@ -73,10 +68,10 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
   String finalPosterUrl;
   VideoAdapter adapter;
   Cursor cursor;
-
   private static final String[] FAV_MOVIE_PROJECTION={
       FavouriteMoviesEntry.COLUMN_NAME,
   };
+
   private static  String selection;
   private static String[] selectionArgs;
 
@@ -89,66 +84,67 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
     ActionBar actionBar = getSupportActionBar();
     actionBar.hide();
     movieDetailsBinding = DataBindingUtil.setContentView(this,R.layout.activity_movie_details);
-
     adapter= new VideoAdapter(this,new ArrayList<Video>());
-   // Log.d("O_MY", "VideoList Size="+String.valueOf(adapter.getCount()));
+
+    // Log.d("O_MY", "VideoList Size="+String.valueOf(adapter.getCount()));
     //PlayVideoOnClickListener clickListener=new PlayVideoOnClickListener(this);
-    /*movieDetailsBinding.textViewTrailer1.setOnClickListener(clickListener);
-    movieDetailsBinding.textViewTrailer2.setOnClickListener(clickListener);
-    movieDetailsBinding.playVideoBtn1.setOnClickListener(clickListener);
-    movieDetailsBinding.playVideoBtn2.setOnClickListener(clickListener);*/
+
+    /*
+      movieDetailsBinding.textViewTrailer1.setOnClickListener(clickListener);
+      movieDetailsBinding.textViewTrailer2.setOnClickListener(clickListener);
+      movieDetailsBinding.playVideoBtn1.setOnClickListener(clickListener);
+      movieDetailsBinding.playVideoBtn2.setOnClickListener(clickListener);
+    */
 
     movieDetailsBinding.videosList.setAdapter(adapter);
     movieDetailsBinding.videosList.setExpanded(true);
+    movieDetailsBinding.videosList.clearFocus();
     if (android.os.Build.VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP){
       setUpTransitions();
     }
 
-
     Intent intent=getIntent();
-      Bundle b=intent.getExtras();
-      Movie currentMovie = b.getParcelable("movie");
-      movieTitle = currentMovie.getmMovieTitle();
-      posterId = currentMovie.getmImageResourceID();
-      movieOverView = currentMovie.getmOverView();
-      movieRating = String.valueOf(currentMovie.getmRating());
-      movieReleaseDate = currentMovie.getmDate();
-      id = currentMovie.getmId();
+    Bundle b=intent.getExtras();
+    Movie currentMovie = b.getParcelable("movie");
+
+    movieTitle = currentMovie.getmMovieTitle();
+    posterId = currentMovie.getmImageResourceID();
+    movieOverView = currentMovie.getmOverView();
+    movieRating = String.valueOf(currentMovie.getmRating());
+    movieReleaseDate = currentMovie.getmDate();
+    id = currentMovie.getmId();
     isFav=currentMovie.getmIsFav();
     searchDB.execute();
-
     videosURL=moviesDbURL+"movie/"+id+"/videos"+apiKey;
     reviewsURL=moviesDbURL+"movie/"+id+"/reviews"+apiKey;
 
     SimpleDateFormat dt1 = new SimpleDateFormat("LLL dd,yyyy");
+
     //Formatting date of this form("23-05-2017") to this form ("May 23,2017")
     SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd");
 
     Date date = null;
     try {
       date = dt.parse(movieReleaseDate);
-    } catch (ParseException e) {
-      e.printStackTrace();
+    }catch(ParseException e) {
+        e.printStackTrace();
     }
 
     //Base URL for poster Url
     String basePosterUrl="http://image.tmdb.org/t/p//w185/";
     //Final URL to be passed into Picasso
     finalPosterUrl = basePosterUrl + posterId;
-
     Context context = this;
 
     Picasso
         .with(context)
         .load(finalPosterUrl)
         .into(movieDetailsBinding.poster);
+
     movieRating=movieRating+"/10";
-
     movieDetailsBinding.dateTextView.setText(dt1.format(date));
-
     movieDetailsBinding.titleTextView.setText(movieTitle);
     movieDetailsBinding.overviewTextView.setText(movieOverView);
-
     movieDetailsBinding.ratingTextView.setText(movieRating);
 
     Bitmap bm = null;
@@ -158,12 +154,12 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
     ConnectivityManager connMgr =
         (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
     NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-    if (networkInfo != null && networkInfo.isConnected()) {
+    if(networkInfo != null && networkInfo.isConnected()) {
       android.app.LoaderManager loaderManager = getLoaderManager();
       loaderManager.initLoader(LOADER_ID, null, this);
     }
 
-        movieDetailsBinding.markAsFavButton.setOnClickListener(new OnClickListener() {
+      movieDetailsBinding.markAsFavButton.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
         if (isFav==0) {
@@ -173,7 +169,6 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
           movieDetailsBinding.markAsFavButton.setBackground(getResources().getDrawable(white_circle));
           addToDb();
           isFav = 1;
-
         }
         else if(isFav==1){
           String message=movieTitle+getString(R.string.removed_from_fav);
@@ -185,7 +180,6 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
         }
       }
     });
-
   }
 
   private void colorizeFromImage(final Bitmap image) {
@@ -197,16 +191,18 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
   }
 
   private void setUpTransitions() {
+
     Slide slide=new Slide(Gravity.BOTTOM);
     slide.excludeTarget(android.R.id.statusBarBackground,true);
     getWindow().setEnterTransition(slide);
     getWindow().setSharedElementsUseOverlay(false);
+    getWindow().setTransitionBackgroundFadeDuration(2000);
   }
+
   private void removeFromFav() {
     Uri uri= FavouriteMoviesEntry.CONTENT_URI;
     uri=uri.buildUpon().appendPath(id).build();
     getContentResolver().delete(uri,null,null);
-
   }
 
   private void addToDb() {
@@ -217,16 +213,15 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
     contentValues.put(FavouriteMoviesEntry.COLUMN_RATING,movieRating);
     contentValues.put(FavouriteMoviesEntry.COLUMN_OVERVIEW,movieOverView);
     contentValues.put(FavouriteMoviesEntry.COLUMN_MOVIE_ID,id);
-
     getContentResolver().insert(FavouriteMoviesEntry.CONTENT_URI,contentValues);
   }
 
-  @Override
-  public Loader<ArrayList<ArrayList<String>>> onCreateLoader(int id,
-      Bundle args) {
+    @Override
+    public Loader<ArrayList<ArrayList<String>>> onCreateLoader(int id,
+        Bundle args) {
+      return new MovieDetailsLoader(this,videosURL,reviewsURL);
+    }
 
-    return new MovieDetailsLoader(this,videosURL,reviewsURL);
-  }
   AsyncTask<Void,Void,Cursor> searchDB= new AsyncTask<Void, Void, Cursor>() {
     @Override
     protected Cursor doInBackground(Void... params) {
@@ -245,7 +240,6 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
           movieDetailsBinding.markAsFavButton.setImageResource(R.drawable.ic_favorite_red_a700_36dp);
           movieDetailsBinding.markAsFavButton.setBackground(getResources().getDrawable(white_circle));
         }
-
       }
     }
   };
@@ -257,10 +251,10 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
     setUI();
   }
 
-
   private void setUI() {
     int j = 0;
     int s = sReviews.size();
+
     while (s>0) {
       ReviewsTotal = ReviewsTotal
           +"<p></br>"
@@ -269,31 +263,32 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
       j++;
       s--;
     }
+
     int t = sTrailers.size();
     Log.d("O_MY", String.valueOf(t));
-    /*  movieDetailsBinding.textViewTrailer1.setTag(sTrailers.get(0));
+
+      /*
+      movieDetailsBinding.textViewTrailer1.setTag(sTrailers.get(0));
       movieDetailsBinding.playVideoBtn1.setTag(sTrailers.get(0));
-
-
       if (t >= 2) {
         movieDetailsBinding.textViewTrailer2.setTag(sTrailers.get(1));
         movieDetailsBinding.playVideoBtn2.setTag(sTrailers.get(1));
       } else {
         movieDetailsBinding.textViewTrailer2.setVisibility(View.INVISIBLE);
         movieDetailsBinding.markAsFavButton.setVisibility(View.INVISIBLE);
-      }*/
+      }
+    */
     for(int k=0;k<t;k++){
       String str=sTrailers.get(k);
       String[] s1=str.split("`");
 
       adapter.add(new Video(s1[0],s1[1]));
       Log.d("O_MY","Added a video of pos="+k);
-     // String imgUrl = "http://img.youtube.com/vi/"+sTrailers.get(0)+ "/0.jpg";
+      //String imgUrl = "http://img.youtube.com/vi/"+sTrailers.get(0)+ "/0.jpg";
       //ImageButton img=(ImageButton)findViewById(R.id.videoThumbNail);
       //Picasso.with(this).load(imgUrl).into(img);
     }
     movieDetailsBinding.textViewReviews.setText(Html.fromHtml(ReviewsTotal));
-
   }
 
   @Override
@@ -301,6 +296,4 @@ public class MovieDetailsActivity extends AppCompatActivity implements LoaderMan
     sTrailers=null;
     sReviews=null;
   }
-
-
 }
